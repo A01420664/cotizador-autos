8cd072c663a3269bdd608c1487445ee2c2f7d13c
gil.morales <gil.morales@bbva.com>
2021-06-17 18:09:38 -0500

    Quitando archivos

--- src/main/java/com/API/cotizador/repositories/RangosAutosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.API.cotizador.repositories;
-
-import com.API.cotizador.collections.RangosAutosCollection;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RangosAutosRepository extends MongoRepository<RangosAutosCollection, Integer> {
-}
--- src/main/java/com/API/cotizador/repositories/RangosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.API.cotizador.repositories;
-
-import com.API.cotizador.collections.RangoCollection;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RangosRepository extends MongoRepository<RangoCollection, String> {
-}
